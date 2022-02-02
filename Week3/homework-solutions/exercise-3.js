855aaa0b57a530ad7b98dbd080328804039f88af
Reema <Reemaalshohof88@gmail.com>
2022-02-02 15:17:18 +0100

    edit the injection function/ex-3

--- Week3/homework-solutions/exercise-3.js
+++ Week3/homework-solutions/exercise-3.js
@@ -9,13 +9,13 @@ const connection = mysql.createConnection({
 
 //1- Give an example of a value that can be passed as name and code that would take advantage of SQL-injection and (fetch all the records in the database).
 //Possible SQL statements to inject the database:
-//-- "SELECT Population FROM Country WHERE Name = 'x' OR 1=1 AND code = 'x' OR 1=1",
-//-- "SELECT Population FROM Country WHERE Name ='' OR ''='' AND code ='' OR ''=''",
-//-- "SELECT Population FROM Country WHERE Name LIKE '%';-- AND code ='the value here doesn't matter' "
+//-- "SELECT Population FROM Country WHERE Name = x' OR '1'='1 AND code = x' OR '1'='1",
+//-- "SELECT Population FROM Country WHERE Name =' OR ''=' AND code =' OR ''=' ",
+//-- "SELECT Population FROM Country WHERE Name LIKE %';-- AND code ='the value here doesn't matter "
 
 // function getPopulation(Country, name, code, cb) {
 //     connection.query(
-//         `SELECT Population FROM ${Country} WHERE Name = ${name} and code = ${code}`,
+//         `SELECT Population FROM ${Country} WHERE Name = '${name}' and code = '${code}'`,
 //         function(err, result) {
 //             if (err) cb(err);
 //             if (result.length == 0) cb(new Error("Not found"));
@@ -23,9 +23,9 @@ const connection = mysql.createConnection({
 //         }
 //     );
 // }
-// getPopulation("Country", "'x' OR 1=1", "'x' OR 1=1", console.log);
-//
-//
+
+//getPopulation("Country", "x' OR '1'='1", "x' OR '1'='1", console.log);
+
 //2.Rewrite the function so that it is no longer vulnerable to SQL injection
 function getPopulation(Country, name, code, cb) {
     connection.query(
